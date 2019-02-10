d01ab82fd656652354afcbde5e5aecc01b65e73d
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-02-10 16:36:46 +0000

    Change to deterministic environment for supervised learning

--- src/experiments/supervised/environment.py
+++ src/experiments/supervised/environment.py
@@ -9,7 +9,7 @@ def _compute_optimal_policy_reward(distance, reward_config, gamma):
     else:
         return reward_config[0] + reward_config[1] * distance
 
-def create_dataset(env, gamma):
+def create_dataset(env, gamma, use_deterministic = True):
     OBS_PER_CONF = 100
     NUMBER_OF_CONF = 100
     for _ in range(NUMBER_OF_CONF):
@@ -22,6 +22,15 @@ def create_dataset(env, gamma):
             last_action = env.last_state['optimal_action']
             if not np.any(last_action):
                 last_action = np.ones_like(last_action)
+
+            if use_deterministic:
+                # If we use deterministic policy
+                # We will always take the first action available
+                (act_idx,) = np.where(last_action == 1)
+                last_action[:] = 0
+                last_action[act_idx[0]] = 1
+            else:
+                last_action = last_action / np.sum(last_action)
 
             (optimal_actions,) = np.where(last_action)
 
@@ -36,6 +45,17 @@ def create_dataset(env, gamma):
                 old_reward,
                 old_state)
 
+            optimal_action = env.last_state['optimal_action']
+            if use_deterministic:
+                # If we use deterministic policy
+                # We will always take the first action available
+                (act_idx,) = np.where(optimal_action == 1)
+                optimal_action[:] = 0
+                optimal_action[act_idx[0]] = 1
+            else:
+                optimal_action = optimal_action / np.sum(optimal_action)
+
+
             reward = _compute_optimal_policy_reward(env.last_state['optimal_distance'], env.reward_configuration, gamma)
-            datarow = (env.last_state['image'], env.last_state['goal'],last_action, reward, last_action_frame)
+            datarow = (env.last_state['image'], env.last_state['goal'], optimal_action, reward, last_action_frame)
             yield datarow
