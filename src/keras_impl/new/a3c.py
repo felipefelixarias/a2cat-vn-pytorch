114cecdf3e8e7da38bf526e6a4e17d30ed34d080
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-02-13 17:43:49 +0000

    Add A3C reporting

--- src/keras_impl/new/a3c.py
+++ src/keras_impl/new/a3c.py
@@ -1,9 +1,10 @@
 import tensorflow as tf
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 from environment.environment import create_env, get_action_space_size
 import numpy as np
 from multiprocessing import Queue, Process
 from model import create_model
+from functools import partial
 import time
 import sys
 
@@ -32,10 +33,54 @@ class MultiprocessingExceptionWrapper:
 
         return True
 
+
+class MetricsReporter:
+    def __init__(self, queue):
+        self._queue = queue
+
+    def report_episode(self, **kwargs):
+        self._queue.put(('episode', kwargs))
+
+class MetricsCollector:
+    def __init__(self):
+        self._total_episodes = 0
+        self._report_episode_frequency = 10
+        self._queue = Queue(maxsize = 10000)
+
+        self._episode_values = defaultdict(list)
+        pass
+
+    def create_reporter(self):
+        return MetricsReporter(self._queue)
+
+    def _try_report(self, global_t):
+        if self._total_episodes % self._report_episode_frequency == 0:
+            self._report(global_t)
+
+    def _report(self, global_t):
+        means = {key: np.mean(x) for key,x in self._episode_values.items()}
+        mean_reward = means['reward']
+        mean_length = means['episode_length']
+        print('Time %s, Episodes %s, Avg. reward: %f, Avg. length %s' % (global_t, self._total_episodes, mean_reward, mean_length))
+
+
+    def handle(self, global_t):
+        while not self._queue.empty():
+            (kind, data) = self._queue.get()
+            if kind != 'episode':
+                raise Exception('Unsupported metric type')
+
+            for key,val in data.items():
+                self._episode_values[key].append(val)
+            
+            self._total_episodes += 1
+            self._try_report(global_t)
+
+
 class AgentProcess(Process):
-    def __init__(self, mode, id,  predictor, training_queue, error_queue, env_args):
+    def __init__(self, mode, id,  predictor, training_queue, error_queue, reporter, env_args):
         super().__init__(name = 'agent-%s' % str(id))
-        daemon = True
+        self.daemon = True
         self.exit_flag = False
         self._env_args = env_args
         self._env = None
@@ -45,6 +90,7 @@ class AgentProcess(Process):
         self._max_episode_length = None
         self._error_queue = error_queue
         self._predictor = predictor
+        self._reporter = reporter
         self.id = id
         pass
 
@@ -66,7 +112,8 @@ class AgentProcess(Process):
         return action
 
     def _create_batch(self, data, next_value):
-        return (data, next_value)
+        stacked_data = list(map(lambda *x: np.stack(x, 0), *data))
+        return stacked_data + [next_value]
 
     def _too_long(self, length):
         return self._max_episode_length is not None and self._max_episode_length <= length
@@ -87,7 +134,7 @@ class AgentProcess(Process):
             state, reward, done, _ = self._env.step(action)
             total_reward += reward
             sub_episode_length += 1
-            sub_episode_data.append([old_state, action, reward, done])
+            sub_episode_data.append(old_state + [action, reward, done])
             total_episode_length += 1
 
             if done or sub_episode_length >= self._max_subepisode_length:
@@ -97,14 +144,14 @@ class AgentProcess(Process):
                     next_value = 0.0
 
                 train_batch = self._create_batch(sub_episode_data, next_value)
-                yield train_batch
+                yield (train_batch, sub_episode_length,)
                 sub_episode_data = []
                 sub_episode_length = 0
 
                 #Context switch
                 time.sleep(0.01)
-
-        return (total_episode_length, total_reward)
+        
+        self._reporter.report_episode(reward = total_reward, episode_length = total_episode_length)
 
 
 class AgentThreadError(Exception):
@@ -117,7 +164,6 @@ class Trainer:
     def __init__(self, total_steps, model_kwargs, num_agents = 32, device = '/gpu:0'):
         self._num_agents = num_agents
         self._eval_request_queue = Queue(maxsize = num_agents)
-        self._eval_request_queue = Queue(maxsize = num_agents)
         self._eval_response_queues = [Queue(maxsize = 8) for _ in range(num_agents)]
         self._error_queue = Queue(maxsize = num_agents)
         self._training_queue = Queue(maxsize = num_agents)
@@ -126,15 +172,15 @@ class Trainer:
         self._eval_batch_size = num_agents // 2
         self._env_args = dict(env_type = 'maze', env_name = 'gr')
         self._mode = 'train'
+        self._metrics_collector = MetricsCollector()
 
         self._agents = []
-
         self._model = create_model(device = device, name = 'net', **model_kwargs)
 
     def _spawn(self):
         for i in range(self._num_agents):
             predictor = Predictor(i, get_action_space_size(**self._env_args), self._eval_request_queue, self._eval_response_queues[i])
-            thread = AgentProcess(self._mode, i, predictor, self._training_queue, self._error_queue, self._env_args)
+            thread = AgentProcess(self._mode, i, predictor, self._training_queue, self._error_queue, self._metrics_collector.create_reporter(), self._env_args)
 
             self._agents.append(thread)
             thread.start()
@@ -148,11 +194,11 @@ class Trainer:
 
         # TODO: implement batching!!
         while not self._training_queue.empty() and batch_size < max_batch_size:
-            (data, lastval, t) = self._training_queue.get_nowait()
-            self._model.set_step(self._learning_rate(global_t), global_t)
-            self._model.train_on_batch(data + [lastval])
+            (data, delta_t) = self._training_queue.get_nowait()
+            self._model.set_step([self._learning_rate(global_t), global_t])
+            self._model.train_on_batch(data)
             batch_size += 1
-            global_t += t
+            global_t += delta_t
 
         return global_t
 
@@ -173,10 +219,10 @@ class Trainer:
         if batch_size > 0:
             result = self._model.run_base_policy_and_value(batch_request)
             for i in range(len(thread_ids)):
-                self._eval_response_queues[thread_ids[i]].put(*[x[i] for x in result])
+                self._eval_response_queues[thread_ids[i]].put(tuple([x[i] for x in result]))
    
     def _handle_statistics(self, global_t):
-        # TODO: implement
+        self._metrics_collector.handle(global_t)
         pass
 
     def _handle_errors(self):
@@ -196,21 +242,19 @@ class Trainer:
         global_t = 0
         try:
             while global_t < self._total_steps:
+                # Takes care of rethrowing errors from child
                 self._handle_errors()
+
+                # Set the learning rate and global t
                 self._model.set_step([learning_rate, global_t])
 
                 # We first handle all training requests
-                self._handle_errors()
                 global_t = self._handle_training(global_t)
 
-                # Then we handle evaluation requests
-                self._handle_errors()
+                # Run the prediction
                 self._handle_evaluation()
 
-                self._handle_errors()
                 self._handle_statistics(global_t)
-
-                self._handle_errors()
 
             # TODO: save model
 
