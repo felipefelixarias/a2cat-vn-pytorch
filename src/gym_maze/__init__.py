a775c90c9c8961ae7f0c2de308d6f653530b0d6f
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-02-16 14:24:02 +0000

    Change environment max steps

--- src/gym_maze/__init__.py
+++ src/gym_maze/__init__.py
@@ -4,7 +4,7 @@ from gym_maze.env import MazeEnv, GoalMazeEnv
 register(
     id='Maze-v0',
     entry_point='gym_maze:MazeEnv',
-    max_episode_steps = 100,
+    max_episode_steps = 200,
 )
 
 register(
