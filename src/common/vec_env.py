bdcb36e4bdcf9e225988122b5e09d56e6db840c1
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-02-19 19:24:01 +0000

    add a2c implementation

--- src/common/vec_env.py
+++ src/common/vec_env.py
@@ -113,7 +113,7 @@ class VecEnv(ABC):
 
 def worker(remote, parent_remote, env_fn_wrapper):
     parent_remote.close()
-    env = env_fn_wrapper.x()
+    env = env_fn_wrapper()
     try:
         while True:
             cmd, data = remote.recv()
@@ -155,7 +155,7 @@ class SubprocVecEnv(VecEnv):
         self.closed = False
         nenvs = len(env_fns)
         self.remotes, self.work_remotes = zip(*[Pipe() for _ in range(nenvs)])
-        self.ps = [Process(target=worker, args=(work_remote, remote, CloudpickleWrapper(env_fn)))
+        self.ps = [Process(target=worker, args=(work_remote, remote, env_fn))
                    for (work_remote, remote, env_fn) in zip(self.work_remotes, self.remotes, env_fns)]
         for p in self.ps:
             p.daemon = True  # if the main process crashes, we should not cause things to hang
