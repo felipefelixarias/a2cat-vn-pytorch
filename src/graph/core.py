3aceee553d84b20260b6e1aaf2dce0817c8897c7
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-02-21 13:45:19 +0000

    Add graph env

--- src/graph/core.py
+++ src/graph/core.py
@@ -1,7 +1,11 @@
 from abc import ABC, abstractproperty, abstractclassmethod
+import numpy as np
 class GridWorldScene:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.graph = None
+        self.optimal_distances = None
 
     @abstractproperty
     def maze(self):
@@ -10,3 +14,11 @@ class GridWorldScene:
     @abstractclassmethod
     def render(self, position, rotation):
         pass
+
+    @property
+    def observation_shape(self):
+        return (81, 81, 3)
+
+    @property
+    def dtype(self):
+        return np.uint8
--- src/graph/env.py
+++ src/graph/env.py
@@ -1,8 +1,55 @@
 import gym
-import gym.envs
-from graph.util import step, is_valid_state, find_state, enumerate_states
-
+import gym.spaces
+from graph.util import step, is_valid_state, load_graph, enumerate_positions, sample_initial_position
+import numpy as np
+import random
 
 class GraphEnv(gym.Env):
-    def __init__(self, graph):
-        self.observation_space = 
+    def __init__(self, graph, goal):
+        self.goal = goal
+        
+        if isinstance(graph, str):
+            with open(graph, 'rb') as f:
+                self.graph = load_graph(f)
+        else:
+            self.graph = graph
+
+        if graph.dtype == np.float32:
+            self.observation_space = gym.spaces.Box(0.0, 1.0, graph.observation_shape, graph.dtype)
+        elif graph.dtype == np.uint8:
+            self.observation_space = gym.spaces.Box(0, 255, graph.observation_shape, graph.dtype)
+        else:
+            raise Exception('Unsupported observation type')
+
+        self.action_space = gym.spaces.Discrete(4)
+        self.state = None
+        self.unwrapped = self
+        self.largest_distance = np.max(self.graph.distances)
+        self.complexity = None
+
+    def set_complexity(self, complexity = None):
+        self.complexity = None
+
+    def reset(self):
+        optimal_distance = None
+        if self.complexity is not None:
+            optimal_distance = self.complexity * (self.largest_distance - 1) + 1
+        state = sample_initial_position(self.graph, self.goal, optimal_distance = optimal_distance)
+        self.state = state
+
+    def observe(self, state):
+        return self.graph.render(self.state[:2], self.state[2])
+
+    def step(self, action):
+        nstate = step(self.state, action)
+        if not is_valid_state(self.graph.maze, nstate):
+            # We can either end the episode with failure
+            # Or continue with negative reward
+            return self.observe(self.state), 0.0, False, dict(state = self.state)
+
+        else:
+            self.state = nstate
+            if self.state[:2] == self.goal:
+                return self.observe(self.state), 1.0, True, dict(state = self.state, win = True)
+            else:
+                return self.observe(self.state), 0.0, False, dict(state = self.state)
