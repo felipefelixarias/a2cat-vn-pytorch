5364186c2e4ebbb8f3424808a374cea871572a23
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-02-25 15:58:26 +0000

    Increase dungeon model capacity

--- src/experiments/util.py
+++ src/experiments/util.py
@@ -7,22 +7,51 @@ def display_q(trainer, fig = None):
     env = trainer.env
 
     old_state = env.unwrapped.state
-    old_selected_graph = env.unwrapped.graph_number
-    maze = env.unwrapped.graphs[0].maze
-    shape = maze.shape
-    env.unwrapped.graph_number = 0
+
+    if hasattr(env.unwrapped, 'graph_number'):
+        old_selected_graph = env.unwrapped.graph_number
+        maze = env.unwrapped.graphs[0].maze
+        env.unwrapped.graph_number = 0
+        is_multi = True
+    else:
+        maze = env.unwrapped.graph.maze
+        is_multi = False
+    shape = maze.shape    
     Q = np.zeros(shape + (4,), dtype = np.float32)
     for y in range(shape[0]):
         for x in range(shape[1]):
+            env.reset()
             env.unwrapped.state = (y, x)
             observation, _, _, _ = env.step(None)
             Q[y, x,:] = trainer._q(observation[None])[0]
+    env.reset()
     env.unwrapped.state = old_state
-    env.unwrapped.graph_number = old_selected_graph
+
+    if is_multi:
+        env.unwrapped.graph_number = old_selected_graph
                 
     V = np.max(Q, 2)
     policy = np.argmax(Q, axis = 2)
     show_policy_and_value(V, policy, maze, fig)
+    return
+
+def display_policy_value(a2ctrainer, fig = None):
+    env = a2ctrainer.validation_env
+    old_state = env.unwrapped.state
+    maze = env.unwrapped.graph.maze
+    shape = maze.shape
+    policy = np.zeros(shape, dtype = np.int32)
+    value = np.zeros(shape, dtype = np.float32)
+    for y in range(shape[0]):
+        for x in range(shape[1]):
+            env.unwrapped.state = (y, x)  
+            env.reset()          
+            obs, _, _, _ = env.step(None)
+            policy[y, x], value[y, x] = a2ctrainer._eval_pv(obs)
+
+    env.reset()
+    env.unwrapped.state = old_state
+    show_policy_and_value(value, policy, maze, fig)
     return
 
 def show_policy_and_value(v, policy, maze, fig):
