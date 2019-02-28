761c4d641f8cc85c88ad77d9b9a2e73994701f0f
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-02-28 16:12:16 +0000

    Auxiliary task implementation

--- src/a2c/auxiliary.py
+++ src/a2c/auxiliary.py
@@ -1,26 +1,17 @@
-from abc import ABC, abstractclassmethod
+from abc import ABC, abstractclassmethod, abstractproperty
+from collections import defaultdict
+
 class AuxiliaryTask(ABC):
-    def __init__(self, name):
+    def __init__(self, name, *args, **kwargs):
         self.name = name
 
-    @abstractclassmethod
-    def build_heads(self, model):
+    @abstractproperty
+    def head_names(self):
         pass
 
     @abstractclassmethod
-    def build_optimization_placeholders(self):
+    def build_loss(self, heads, feed_builder):
         pass
-
-    @abstractclassmethod
-    def build_loss(self, heads, placeholders):
-        pass
-
-    @abstractclassmethod
-    def bind_input(self, observation):
-        pass
-
-    def wrap_env(self, env):
-        return env
 
 
 task_registry = dict()
@@ -31,3 +22,108 @@ def register_auxiliary_task(name):
         )
 
     return reg
+
+def _create_auxiliary_task(name, **kwargs):
+    return task_registry[name]['task'](**kwargs)
+
+
+def build_auxiliary_losses(heads, names, feed_builder, **kwargs):
+    heads = dict(zip(heads, names))
+    losses = dict()
+    for name in task_registry.keys():
+        aux_task = _create_auxiliary_task(name, **kwargs)
+        task_heads = []
+        for head_name in aux_task.head_names:
+            hname = name + '_' + head_name
+            if not hname in heads:
+                raise Exception('Missing head %s' % hname)
+
+            task_heads.append(heads[hname])
+
+        loss = aux_task.build_loss(task_heads, feed_builder)
+        losses[name] = loss
+
+    return losses
+
+def merge_auxiliary_losses(losses, weights = defaultdict(lambda: 1.0)):
+    total_loss = 0.0
+    for key, loss in losses.items():
+        total_loss += weights[key] * loss
+    return total_loss
+
+import trfl
+import keras.backend as K
+import tensorflow as tf
+
+@register_auxiliary_task('pixel_control')
+class PixelControl(AuxiliaryTask):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cell_size = (4, 4)
+
+    @property
+    def head_names(self):
+        return ['main']
+
+    def build_loss(self, heads, feed_builder):
+        head = heads[0][:, -1]
+        actions = feed_builder.get('actions')
+        observations = feed_builder.get('observations')
+        terminals = feed_builder.get('terminals')
+        number_of_actions = int(head.get_shape()[-1])
+        actions_expanded = K.one_hot(K.expand_dims(K.expand_dims(actions, 2), 3), number_of_actions)
+
+        q_prediction = head * actions_expanded
+        q_true = tf.transpose(
+            trfl.pixel_control_rewards(
+                tf.transpose(observations, [1, 0, 2, 3, 4]), 
+                self.cell_size
+            ),
+            [1, 0, 2, 3]
+        )
+
+        loss = tf.nn.l2_loss((q_true - q_prediction) * (1.0 - tf.to_float(terminals)))
+        return tf.reduce_mean(loss)
+
+@register_auxiliary_task('reward_prediction')
+class RewardPrediction(AuxiliaryTask):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @property
+    def head_names(self):
+        return ['main']
+
+    def build_loss(self, heads, feed_builder):
+        head = heads[0][:, -1]
+        rewards = feed_builder.get('rewards')
+        terminals = feed_builder.get('terminals')
+        r_true = tf.stack([rewards == 0, rewards > 0, rewards < 0], axis = -1)
+        loss = tf.nn.softmax_cross_entropy_with_logits(head, r_true)
+        loss = loss * (1.0 - tf.to_float(terminals))
+        return tf.reduce_mean(loss)
+
+@register_auxiliary_task('value_prediction')
+class ValuePrediction(AuxiliaryTask):
+    def __init__(self, gamma, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.gamma = gamma
+
+    @property
+    def head_names(self):
+        return ['main', 'next_value']
+
+    def build_loss(self, heads, feed_builder):
+        head = heads[0][:, -1]
+        rewards = feed_builder.get('rewards')
+        terminals = feed_builder.get('terminals')
+        base_value = feed_builder.value[:, -1]
+        pcontinues = self.gamma * (1.0 - tf.to_float(terminals))
+        cummulative_rewards = trfl.scan_discounted_sum(
+            tf.transpose(rewards),
+            tf.transpose(pcontinues),
+            base_value, 
+            reverse=True,
+            name='cummulative_reward')
+        cummulative_rewards = tf.transpose(cummulative_rewards)
+        return tf.nn.l2_loss(head - cummulative_rewards)
