6b4bd910262ab845f007b0e3866181ec6b77b9fc
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-02-28 20:04:39 +0000

    Fix validation and saving periods

--- src/experiments/pong_a2c.py
+++ src/experiments/pong_a2c.py
@@ -19,7 +19,7 @@ class FlatWrapper(gym.ObservationWrapper):
 
 
 register_agent('pong-a2c')(A2CAgent)
-@register_trainer('pong-a2c', max_time_steps = 10e6, validation_period = 1000,  episode_log_interval = 100, saving_period = 100000)
+@register_trainer('pong-a2c', max_time_steps = 10e6, validation_period = 100,  episode_log_interval = 10, saving_period = 500000)
 class Trainer(A2CTrainer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
