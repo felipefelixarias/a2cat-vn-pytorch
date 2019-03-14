9b69423fb8c820a9e15921048f1845c2354b06fc
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-03-14 09:44:12 +0000

    Add thor grid experiment

--- environments/gym_graph/__init__.py
+++ environments/gym_graph/__init__.py
@@ -3,7 +3,7 @@ from .download import available_scenes
 
 for key, name in available_scenes():
     gym.register(
-        id = key + '-v0',
+        id = 'Graph' + key + '-v0',
         entry_point = 'environments.gym_graph.graph:OrientedGraphEnv',
         max_episode_steps = 100,
         kwargs = dict(
