35b2ea08a1ff00a7e7fd5690d05fc9f168d6f2f5
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-03-15 18:04:41 +0000

    Train ai2thor hardware acceleration

---
+++ train.sh
@@ -0,0 +1,31 @@
+# !/bin/bash
+apt-get -qqy install xserver-xorg-core xserver-xorg-video-dummy libxcursor1 x11vnc unzip pciutils software-properties-common kmod gcc make linux-headers-generic wget
+
+cat << EOF > /root/xorg.conf
+Section "Device"
+    Identifier     "Device0"
+    Driver         "nvidia"
+    VendorName     "NVIDIA Corporation"
+    BusID          "$AI2THOR_DEVICE_BUSID"
+EndSection
+Section "Screen"
+    Identifier     "Screen0"
+    Device         "Device0"
+    DefaultDepth    24
+    Option         "AllowEmptyInitialConfiguration" "True"
+    SubSection     "Display"
+        Depth       24
+        Virtual 1024 768
+    EndSubSection
+EndSection
+Section "ServerLayout"
+    Identifier     "Layout0"
+    Screen 0 "Screen0" 0 0
+EndSection
+EOF
+
+
+Xorg -noreset -logverbose -logfile xorg.log -config /root/xorg.conf :0&
+sleep 1
+# x11vnc -display :0 -rfbauth /root/.vnc/passwd&
+DISPLAY=:0.0 python3 train.py $1
