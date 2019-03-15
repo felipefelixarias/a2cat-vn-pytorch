26fd23e0cc2be8c86023228cb23ce9a8007ceb16
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-03-15 19:26:21 +0000

    Optimize thor experiments

--- environments/gym_ai2thor/__init__.py
+++ environments/gym_ai2thor/__init__.py
@@ -13,6 +13,6 @@ for i in range(1, NUMBER_OF_SCENES + 1):
     gym.register(
         id = 'DiscreteThor%s-v0' % i,
         entry_point = 'environments.gym_ai2thor.envs.discrete:DiscreteEnv',
-        max_episode_steps = 150,
+        max_episode_steps = 300,
         kwargs = dict(scene_id = i)
     )
