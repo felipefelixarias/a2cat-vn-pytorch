b56ad1f4588bb5bf5e88f4172cb7382658da42ed
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-03-18 02:37:04 +0000

    House single goal experiment

--- environments/gym_house/env.py
+++ environments/gym_house/env.py
@@ -3,7 +3,9 @@ from deep_rl.configuration import configuration
 from House3D import objrender, Environment
 from House3D.roomnav import RoomNavTask
 import numpy as np
+import random
 import os
+import cv2
 
 def create_configuration():
     path = configuration.get('house3d.framework_path')
@@ -15,18 +17,28 @@ def create_configuration():
     }
 
 class GymHouseWrapper(gym.ObservationWrapper):
-    def __init__(self, env):
+    def __init__(self, env, room_types = None, screen_size = None):
         super().__init__(env)
+        self.screen_size = screen_size
+        self.room_types = room_types
 
     def observation(self, observation):
-        print(observation.shape)
-        return observation
+        return cv2.resize(observation, self.screen_size, interpolation = cv2.INTER_CUBIC)
 
-def GymHouseEnv(scene = '05cac5f7fdd5f8138234164e76a97383', screen_size = (84,84)):
+    def reset(self):
+        goals = set(self.env.house.all_desired_roomTypes)
+        if self.room_types is not None:
+            goals.intersection_update(set(self.room_types))
+
+        target = random.choice(list(goals))
+
+        return self.observation(self.env.reset(target))
+
+def GymHouseEnv(scene = '05cac5f7fdd5f8138234164e76a97383', screen_size = (84,84), goals = ['bedroom']):
     h, w = screen_size
     api = objrender.RenderAPI(w = 300, h = 300, device = 0)
     env = Environment(api, scene, create_configuration())
     env.reset()
     env = RoomNavTask(env, discrete_action = True, depth_signal = False, segment_input = False)
     env.observation_space.dtype = np.uint8
-    return GymHouseWrapper(env)
+    return GymHouseWrapper(env, room_types=goals, screen_size = screen_size)
