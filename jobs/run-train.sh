d6f69bc46698450e91fa180359b4439c06a1d332
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-03-20 22:44:30 +0000

    Fix runtrain shell script

--- jobs/run-train.sh
+++ jobs/run-train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cat ~/experiments/target-driven-visual-navigation/jobs/job-template.sh | sed "s/{jobname}/$1/g" | tee job.sh
+cat ~/experiments/target-driven-visual-navigation/jobs/job-template.sh | sed "s/{jobname}/$1/g" | tee job.sh > /dev/null
 sbatch job.sh
 rm job.sh
