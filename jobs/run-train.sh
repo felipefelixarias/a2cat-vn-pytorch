d0061aa5c8c20e5eaf4363f5ccadd6a19b86d00d
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-03-20 19:13:40 +0000

    Conditional run script

--- jobs/run-train.sh
+++ jobs/run-train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cat ~/experiments/target-driven-visual-navigation/jobs/job-template.sh | sed "s/{jobname}/$1/g" | '__job__.sh'
-sbatch '__job__.sh'
+cat ~/experiments/target-driven-visual-navigation/jobs/job-template.sh | sed "s/{jobname}/$1/g" | tee '__job__.sh' && \
+sbatch '__job__.sh' && \ 
 rm '__job__.sh'
