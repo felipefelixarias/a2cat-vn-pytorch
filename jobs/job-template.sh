417bcd010ea55c7e913c01eee2c1dcda56c0af42
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-03-20 15:38:25 +0000

    Change job template

--- jobs/job-template.sh
+++ jobs/job-template.sh
@@ -3,13 +3,20 @@
 #SBATCH --job-name=chouse-single-goal-hard-bignet
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --time=2-00:00
+#SBATCH --time=48:00:00
 #SBATCH --mem=12G
 #SBATCH --gres=gpu:1
 
 TASKNAME=chouse-single-goal-hard-bignet
 
 ml load singularity
+SCRATCH_DIRECTORY=/lscratch/${USER}/${SLURM_JOBID}.stallo-adm.uit.no
+mkdir -p ${SCRATCH_DIRECTORY}
+cd ${SCRATCH_DIRECTORY}
+git init
+git remote add origin git@github.com:jkulhanek/target-driven-visual-navigation.git
+git pull origin master
+
 cd ~
 singularity exec images/jkulhanek
-singularity exec --nv images/jkulhanek-target-driven-visual-navigation-master-latest.simg python3 ~/experiments/target-driven-visual-navigation/train.py $TASKNAME
+singularity exec -B ${SCRATCH_DIRECTORY}:/experiment --nv images/jkulhanek-target-driven-visual-navigation-master-latest.simg python3 ~/experiments/target-driven-visual-navigation/train.py $TASKNAME
