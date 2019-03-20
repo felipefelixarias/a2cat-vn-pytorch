6a77f63af59b85d0e9c1bbd7c8c7060e398f55ec
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-03-20 19:08:46 +0000

    Fix path

--- jobs/run-train.sh
+++ jobs/run-train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cat '~/experiments/target-driven-visual-navigation/job-template.sh' | sed "s/{jobname}/$EXPERIMENT/g" | '__job__.sh'
+cat '~/experiments/target-driven-visual-navigation/jobs/job-template.sh' | sed "s/{jobname}/$EXPERIMENT/g" | '__job__.sh'
 sbatch '__job__.sh'
 rm '__job__.sh'
