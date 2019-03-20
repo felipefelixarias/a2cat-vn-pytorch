cc09f4ceff93dc7caa1f283d80e6ef6a3625a517
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-03-20 14:00:48 +0000

    Use job template

--- jobs/job-template.sh
+++ jobs/job-template.sh
@@ -1,5 +1,15 @@
 #/bin/bash
-#SBATCH --job-name=target-driven-visual-navigation
-#SBATCH --ntasks=20
-#SBATCH --time=0-00:05:00
-#SBATCH --mem-per-cpu=500MB
+
+#SBATCH --job-name=chouse-single-goal-hard-bignet
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --time=2-00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=120G
+#SBATCH --gres=gpu:1
+
+TASKNAME=chouse-single-goal-hard-bignet
+
+ml load singularity
+singularity exec images/jkulhanek
+singularity exec --nv images/jkulhanek-target-driven-visual-navigation-master-latest.simg python3 ~/experiments/target-driven-visual-navigation/train.py $TASKNAME
