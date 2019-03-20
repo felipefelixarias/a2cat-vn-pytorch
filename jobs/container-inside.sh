749eef09798d83f4b239f7d48010a9fc2a11c079
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-03-20 23:07:08 +0000

    Fix taskrun

---
+++ jobs/container-inside.sh
@@ -0,0 +1,12 @@
+echo "Downloading new version of deep-rl-pytorch"
+pip3 install --user git+https://github.com/jkulhanek/deep-rl-pytorch.git
+echo "Verifying mounted repository"
+if [ -e /experiment ]
+then
+    echo "Experiment directory mounted"
+    echo "Container is ready!"
+    echo "Launching experiment with arguments [$@]"
+    python3 "/experiment/$1" "${@:1}"
+else
+    echo "You have to mount your repository to /experiment"
+fi
