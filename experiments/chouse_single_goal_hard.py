2b25f148ee4328a1d1450312e3a135d82c63381e
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-03-20 13:45:55 +0000

    Add hard bignet

--- experiments/chouse_single_goal_hard.py
+++ experiments/chouse_single_goal_hard.py
@@ -166,7 +166,7 @@ def default_args():
             screen_size=(84,84), 
             scene = '05cac5f7fdd5f8138234164e76a97383', 
             goals = ['living_room'], 
-            hardness = 0.6,
+            hardness = 0.9,
             configuration=deep_rl.configuration.get('house3d').as_dict()),
         model_kwargs = dict()
     )
---
+++ jobs/job-template.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+#SBATCH --job-name=target-driven-visual-navigation
+#SBATCH --ntasks=20
+#SBATCH --time=0-00:05:00
+#SBATCH --mem-per-cpu=500MB
