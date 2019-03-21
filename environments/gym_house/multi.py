63283488669c208b1e20fce85e5df8808a811d00
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-03-21 21:30:13 +0000

    Fix multienv experiment

--- environments/gym_house/multi.py
+++ environments/gym_house/multi.py
@@ -2,7 +2,7 @@ from deep_rl.common.env import SubprocVecEnv, DummyVecEnv
 import gym
 
 def create_multiscene(num_processes, scenes, wrap = lambda e: e, **kwargs):
-    assert len(scenes) % num_processes == 0
+    assert len(scenes) % num_processes == 0, "The number of processes %s must devide the number of scenes %s" % (num_processes, len(scenes))
     scenes_per_process = len(scenes) % num_processes
 
     funcs = []
