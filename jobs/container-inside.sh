a1c30c18b4cc7b6979c0b2c8efa2213dd00d694b
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-03-21 00:56:09 +0000

    Fix singularity container

--- jobs/container-inside.sh
+++ jobs/container-inside.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "Downloading new version of deep-rl-pytorch"
-pip3 install --user git+https://github.com/jkulhanek/deep-rl-pytorch.git
+#pip3 install --user git+https://github.com/jkulhanek/deep-rl-pytorch.git
 echo "Verifying mounted repository"
-if [ -e /experiment ]
+if [ -e /opt ]
 then
     echo "Experiment directory mounted"
     echo "Container is ready!"
     echo "Launching experiment with arguments [$@]"
-    python3 "/experiment/$1" "${@:1}"
+    exec python3 "/usr/bin/$@"
 else
     echo "You have to mount your repository to /experiment"
 fi
