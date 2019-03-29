61f1aa55b434b205b31f041205e1dab5bee35c98
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-03-29 14:02:35 +0000

    Add more auxiliary epochs

--- experiments/chouse_multigoal_auxiliary.py
+++ experiments/chouse_multigoal_auxiliary.py
@@ -14,6 +14,9 @@ from deep_rl.model import TimeDistributed, Flatten, MaskedRNN
 from deep_rl.common.tester import TestingEnv, TestingVecEnv
 from models import GoalUnrealModel
 import math
+import torch
+import os
+from deep_rl.configuration import configuration
 
 TestingEnv.set_hardness = lambda _, hardness: print('Hardnes was set to %s' % hardness)
 TestingVecEnv.set_hardness = lambda _, hardness: print('Hardnes was set to %s' % hardness)
@@ -54,7 +57,10 @@ class Trainer(AuxiliaryTrainer):
         return env
 
     def create_model(self):
-        return AuxiliaryBigGoalHouseModel2(self.env.observation_space.spaces[0].spaces[0].shape[0], self.env.action_space.n)
+        model = AuxiliaryBigGoalHouseModel2(self.env.observation_space.spaces[0].spaces[0].shape[0], self.env.action_space.n)
+        model_path = os.path.join(configuration.get('models_path'),'chouse-auxiliary-supervised', 'weights.pth')
+        model.load_state_dict(torch.load(model_path))
+        return model
 
     def process(self, *args, **kwargs):
         a, b, metric_context = super().process(*args, **kwargs)
