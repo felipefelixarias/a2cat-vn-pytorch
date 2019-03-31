cdce01bfd20425c74b758b6089ae1ac68995a96a
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-03-31 14:04:40 +0100

    Add thor experiment

--- environments/gym_graph/__init__.py
+++ environments/gym_graph/__init__.py
@@ -1,6 +1,10 @@
 import gym
 from .download import available_scenes
 
+TASKS = [
+    ('', (12, 3, 0)),
+]
+
 for key, name in available_scenes():
     gym.register(
         id = 'Graph' + key + '-v0',
@@ -14,5 +18,11 @@ for key, name in available_scenes():
 gym.register(
     id = 'OrientedGraph-v0',
     entry_point = 'environments.gym_graph.graph:OrientedGraphEnv',
+    max_episode_steps = 900,
+)
+
+gym.register(
+    id = 'AuxiliaryGraph-v0',
+    entry_point = 'environments.gym_graph.graph:GoalGymGraphAuxiliaryEnv',
     max_episode_steps = 900,
 )
--- environments/gym_graph/graph.py
+++ environments/gym_graph/graph.py
@@ -1,11 +1,13 @@
 import gym
 import numpy as np
 import gym.spaces
+from graph.core import GraphResize
 from graph.util import load_graph, step, sample_initial_state, is_valid_state
 from .download import get_graph
+import random
 
 class OrientedGraphEnv(gym.Env):
-    def __init__(self, graph_name = None, graph_file = None, goal = None, rewards = [1.0, 0.0, 0.0]):
+    def __init__(self, graph_name = None, graph_file = None, goals = None, screen_size = (174,174), rewards = [1.0, 0.0, 0.0]):
         if graph_name is not None:
             self.graph = get_graph(graph_name)
         elif graph_file is not None:
@@ -15,12 +17,13 @@ class OrientedGraphEnv(gym.Env):
         else:
             raise Exception('Not supported')
 
-        if goal is None:
-            self.goal = self.graph.goal
-        else:
-            self.goal = goal
+        self.graph = GraphResize(self.graph, screen_size)
 
-        
+        if goals is None:
+            self.goals = self.graph.goals
+        else:
+            self.goals = goals
+
         if self.graph.dtype == np.float32:
             self.observation_space = gym.spaces.Box(0.0, 1.0, self.graph.observation_shape, np.float32)
         elif self.graph.dtype == np.uint8:
@@ -42,6 +45,8 @@ class OrientedGraphEnv(gym.Env):
         self.complexity = complexity
 
     def reset(self):
+        self.goal = random.choice(self.goals) if isinstance(self.goals, list) else self.goals
+        
         optimal_distance = None
         if self.complexity is not None:
             optimal_distance = self.complexity * (self.largest_distance + 4 - 1) + 1
@@ -53,6 +58,9 @@ class OrientedGraphEnv(gym.Env):
         observation = self.graph.render(state[:2], state[2])
         return observation
 
+    def is_goal(self, state):
+        return max(map(lambda a,b: abs(a - b), state[:2], self.goal[:2])) <= 2 and state[2] == self.goal[2]
+
     def step(self, action):
         nstate = step(self.state, action)
         if not is_valid_state(self.graph.maze, nstate):
@@ -62,7 +70,7 @@ class OrientedGraphEnv(gym.Env):
 
         else:
             self.state = nstate
-            if self.state[:2] == self.goal:
+            if self.is_goal(self.state):
                 return self.observe(self.state), self.rewards[0], True, dict(state = self.state, win = True)
             else:
                 return self.observe(self.state), self.rewards[1], False, dict(state = self.state)
@@ -80,3 +88,30 @@ class OrientedGraphEnv(gym.Env):
             return img
         else:
             raise Exception("Render mode %s is not supported" % mode)
+
+
+class GoalGymGraphAuxiliaryEnv(OrientedGraphEnv):
+    def __init__(self, goals = None, screen_size = (174, 174,), **kwargs):
+        super().__init__(goals = goals, **kwargs)
+
+        self.screen_size = screen_size
+        self.observation_space = gym.spaces.Tuple((
+            self.observation_space,
+            gym.spaces.Box(0, 255, self.screen_size + (3,), dtype = np.uint8),
+            gym.spaces.Box(0, 255, self.screen_size + (1,), dtype = np.uint8),
+            gym.spaces.Box(0, 255, self.screen_size + (3,), dtype = np.uint8),
+            gym.spaces.Box(0, 255, self.screen_size + (3,), dtype = np.uint8)))
+
+        self._cached_goal = (None, None)
+
+    def render_goal(self):
+        cached, value = self._cached_goal
+        if cached is None or cached != self.goal:
+            value = self.graph.render(self.goal[:2], self.goal[2], modes = ['rgb','segmentation'])
+            self._cached_goal = (self.goal, value,)
+        return value
+
+    def observe(self, state):
+        goal_rgb, goal_segmentation = self.render_goal()
+        rgb, depth, segmation = self.graph.render(state[:2], state[2], modes = ['rgb','depth', 'segmentation'])
+        return (rgb, goal_rgb, depth, segmation, goal_segmentation)
