2dc95434578c685c045a0fd01ebbb36a33cdf3dc
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-04-01 12:42:01 +0100

    Chouse final experiment

--- experiments/chouse_final_auxiliary.py
+++ experiments/chouse_final_auxiliary.py
@@ -1,4 +1,4 @@
-from experiments.data import TRAIN2, VALIDATION2
+from experiments.data import TRAIN3, VALIDATION3
 from environments.gym_house.multi import create_multiscene
 from deep_rl.common.env import RewardCollector, TransposeImage, ScaledFloatFrame
 from deep_rl.common.vec_env import DummyVecEnv, SubprocVecEnv
@@ -57,7 +57,7 @@ class Trainer(AuxiliaryTrainer):
 
     def create_model(self):
         model = Model(self.env.observation_space.spaces[0].spaces[0].shape[0], self.env.action_space.n)
-        model_path = os.path.join(configuration.get('models_path'),'chouse-auxiliary4-supervised', 'weights.pth')
+        model_path = os.path.join(configuration.get('models_path'),'chouse-multienv4-auxiliary', 'weights.pth')
         print('Loading weights from %s' % model_path)
         model.load_state_dict(torch.load(model_path))
         return model
@@ -77,9 +77,9 @@ def create_envs(num_training_processes, env_kwargs):
         env = UnrealEnvBaseWrapper(env)
         return env
 
-    env = create_multiscene(num_training_processes, TRAIN2, wrap = wrap, **env_kwargs)
+    env = create_multiscene(num_training_processes, TRAIN3, wrap = wrap, **env_kwargs)
     env.set_hardness = lambda hardness: env.call_unwrapped('set_hardness', hardness)
-    val_env = create_multiscene(VALIDATION_PROCESSES, VALIDATION2, wrap = wrap, **env_kwargs)
+    val_env = create_multiscene(VALIDATION_PROCESSES, VALIDATION3, wrap = wrap, **env_kwargs)
     val_env.set_hardness = lambda hardness: val_env.call_unwrapped('set_hardness', hardness)
     val_env.set_hardness(0.6)
     return env, val_env
