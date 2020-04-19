d310c5b1cb7f581b2ee40b3cbd3da28c6cc50c1b
felipefelixarias <felipefelixarias@gmail.com>
2020-04-18 22:10:27 -0500

    Deleted continous experiments

--- environments/gym_house/multi.py
+++ environments/gym_house/multi.py
@@ -1,5 +1,4 @@
 from deep_rl.common.env import SubprocVecEnv, DummyVecEnv
-from House3D.core import Environment, local_create_house
 import random
 import time
 import gym
@@ -18,59 +17,4 @@ def create_multiscene(num_processes, scenes, wrap = lambda e: e, **kwargs):
     else:
         return SubprocVecEnv(funcs)
 
-
-class MultiHouseEnv(Environment):
-    def __init__(self, api, houses, config, seed=None):
-        """
-        Args:
-            houses: a list of house id or `House` instance.
-        """
-        print('Generating all houses ...')
-        ts = time.time()
-        if not isinstance(houses, list):
-            houses = [houses]
-        _args = [(h, config) for h in houses]
-        self.all_houses = list(map(lambda x: local_create_house(*x), _args))
-        print('  >> Done! Time Elapsed = %.4f(s)' % (time.time() - ts))
-        for i, h in enumerate(self.all_houses):
-            h._id = i
-            h._cachedLocMap = None
-        super(MultiHouseEnv, self).__init__(
-            api, house=self.all_houses[0], config=config, seed=seed)
-
-    def reset_house(self, house_id=None):
-        """
-        Reset the scene to a different house.
-        Args:
-            house_id (int): a integer in range(0, self.num_house).
-                If None, will choose a random one.
-        """
-        if house_id is None:
-            self.house = random.choice(self.all_houses)
-        else:
-            self.house = self.all_houses[house_id]
-        self._load_objects()
-
-    def cache_shortest_distance(self):
-        # TODO
-        for house in self.all_houses:
-            house.cache_all_target()
-
-    @property
-    def info(self):
-        ret = super(MultiHouseEnv, self).info
-        ret['house_id'] = self.house._id
-        return ret
-
-    @property
-    def num_house(self):
-        return len(self.all_houses)
-
-    def gen_2dmap(self, x=None, y=None, resolution=None):
-        # TODO move cachedLocMap to House
-        self.cachedLocMap = self.house._cachedLocMap
-        retLocMap = super(MultiHouseEnv, self).gen_2dmap(x, y, resolution)
-        if self.house._cachedLocMap is None:
-            self.house._cachedLocMap = self.cachedLocMap
-        return retLocMap
 
