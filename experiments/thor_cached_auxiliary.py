0d3f6c692b809633a404a678534962567101bf32
Victor Gonzalez <victor.gonzalez@rice.edu>
2020-05-09 16:47:30 -0500

    update forward pass

--- experiments/thor_cached_auxiliary.py
+++ experiments/thor_cached_auxiliary.py
@@ -52,7 +52,8 @@ class Trainer(AuxiliaryTrainer):
         return env
 
     def create_model(self):
-        model = Model(self.env.observation_space.spaces[0].spaces[0].shape[0], self.env.action_space.n, 4, True)
+        # model = Model(self.env.observation_space.spaces[0].spaces[0].shape[0], self.env.action_space.n, 4, True)
+        model = Model(3, self.env.action_space.n, 4, True)
         return model
 
 def create_envs(num_training_processes, tasks, **env_kwargs):
